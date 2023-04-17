51d2b1781785aad1664e6cd80add047a2d9177c8
김송이 <song3078@naver.com>
2023-04-17 16:54:19 +0900

    5-2직접풀기

---
+++ app/src/main/java/com/example/project5_2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.project5_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("김송이 project5-2");
+    }
+}
